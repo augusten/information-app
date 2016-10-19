245a5cf645e5712a335f991ff57a9d79ed769b4d
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-19 12:15:36 +0200

    Added a create user option that redirects to the list of users

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set( 'views', __dirname + '/views' )
 // GET request to list all the users in JSON file
 app.get( '/users', ( request, response ) => {
 	console.log( 'About to show users' )
-	// response.render( 'index' )
 	fs.readFile( __dirname + '/users.json', ( err, data ) => {
 		if (err) throw err
 		let parsedData = JSON.parse ( data )
@@ -22,12 +21,9 @@ app.get( '/users', ( request, response ) => {
 
 // GET request to search for use in JSON file
 app.get( '/search', ( request, response ) => {
+	response.render( 'search') //{data: parsedData}
 	console.log( 'Someone is about to search for a user' )
-	fs.readFile( __dirname + '/users.json', ( err, data ) => {
-		if (err) throw err
-		let parsedData = JSON.parse ( data )
-		response.render( 'search', {data: parsedData})
-	})
+
 })
 
 // POST request to search for input in JSON file
@@ -41,6 +37,26 @@ app.post('/result', urlencodedParser, function (request, response) {
 		})
 	})
 
+})
+
+// app.get to show a form 
+app.get( '/create', ( req, res ) => {
+	res.render( 'createuser' )
+})
+
+app.post( '/users', urlencodedParser, ( req, res ) => {
+	fs.readFile( __dirname + '/users.json', ( err, data ) => {
+		if (err) throw err
+		let parsedData = JSON.parse ( data )
+		// add condition to add user only if they are not in the file yet
+		parsedData.push( { 
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			email: req.body.email } )
+		// Add to / rewrite existing file with addition of new user
+		fs.writeFile( 'users.json', JSON.stringify( parsedData ))
+		res.render( 'users', {data: parsedData} )
+	})
 })
 
 app.listen( 8000, () => {
