e71e739cb46e3f04d708e3cc3712eb7e8da6a688
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-19 11:33:38 +0200

    finished second route and started the third to create user

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const express = require( 'express' )
-const fs	  = require( 'fs' )
-const app 	  = express()
+const express 	 = require( 'express' )
+const fs	     = require( 'fs' )
+const bodyParser = require( 'body-parser' )
+const app 	     = express()
+
+// include variable to parse input data!
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set( 'view engine', 'pug' )
 app.set( 'views', __dirname + '/views' )
 
-app.get( '/ping', ( request, response ) => {
-	response.send( 'Pong' )
-})
-
-app.get( '/index', ( request, response ) => {
-	console.log( 'About to render a pug page' )
+// GET request to list all the users in JSON file
+app.get( '/users', ( request, response ) => {
+	console.log( 'About to show users' )
 	// response.render( 'index' )
-	fs.readFile( __dirname + '/data.json', ( err, data ) => {
+	fs.readFile( __dirname + '/users.json', ( err, data ) => {
 		if (err) throw err
 		let parsedData = JSON.parse ( data )
-		console.log( parsedData )	
-		response.render( 'index', {data: parsedData})
+		response.render( 'users', {data: parsedData})
 	})
+})
+
+// GET request to search for use in JSON file
+app.get( '/search', ( request, response ) => {
+	console.log( 'Someone is about to search for a user' )
+	fs.readFile( __dirname + '/users.json', ( err, data ) => {
+		if (err) throw err
+		let parsedData = JSON.parse ( data )
+		response.render( 'search', {data: parsedData})
+	})
+})
+
+// POST request to search for input in JSON file
+app.post('/result', urlencodedParser, function (request, response) {
+	fs.readFile( __dirname + '/users.json', ( err, data ) => {
+		if (err) throw err
+		let parsedData = JSON.parse ( data )
+		response.render( 'result', { 
+			data: parsedData, 
+			inputData: [request.body.firstname, request.body.lastname] 
+		})
+	})
+
 })
 
 app.listen( 8000, () => {
